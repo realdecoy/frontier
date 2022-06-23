40440c57b4a43c3373ba2c4fdfe04afbc3ef1d2e
JantaeLeckie <jaleckiie22@gmail.com>
2022-06-23 15:33:24 -0500

    build: plugin-shared catch function and test - update #1

---
+++ packages/frontier-plugins/plugin-shared/test/catch.spec.ts
@@ -0,0 +1,31 @@
+import {expect, test} from '@oclif/test'
+import { exec } from 'child_process'
+import catchFunction from '../src/functions/catch'
+
+
+describe('test the catch function', () => {
+  test
+  .stdout()
+
+  .it("should pass if an error was thrown",  async function () {
+    const testinput = 'an error was thrown';
+    // const error = Error("This is an error")
+
+    try {
+        const caught = await catchFunction(Error("This is an error"))
+        console.log(caught)
+    } catch (error) {
+        expect(testinput).equal('an error was thrown')
+        console.log(error)
+    }
+    // const caught = await catchFunction(Error("This is an error"))    
+    // // console.log(error)
+    // throw new Error("This is an error");
+    
+    // expect(testinput).equal('should display well done')
+  })
+
+})
+
+
+  
