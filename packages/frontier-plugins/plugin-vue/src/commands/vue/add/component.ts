36f6465e33e339bb0a70f8fde6c64f227ceb150f
JantaeLeckie <jaleckiie22@gmail.com>
2022-07-21 15:49:44 -0500

    feat: rebasing branch for pull request26

--- packages/frontier-plugins/plugin-vue/src/commands/vue/add/component.ts
+++ packages/frontier-plugins/plugin-vue/src/commands/vue/add/component.ts
@@ -1,6 +1,6 @@
 import { Command, flags } from '@oclif/command';
 import chalk from 'chalk';
-import { toKebabCase } from '@rdfrontier/stdlib/types/caseConversions';
+import { toKebabCase } from '@rdfrontier/stdlib';
 import { checkProjectValidity, parseComponentName } from '../../../utils/utilities';
 import { CLI_COMMANDS, CLI_STATE, DOCUMENTATION_LINKS } from '../../../utils/constants';
 import { addElementFunction } from '../../../functions/addElement';
