0130b43d6ae14271ef9bba811a2a981064dce7c4
JantaeLeckie <jaleckiie22@gmail.com>
2022-07-21 11:50:06 -0500

    feat: rebasing branch for pull request2

---
+++ packages/frontier-plugins/plugin-shared/src/testCommands/vue-add-commands/addPage.ts
@@ -0,0 +1,49 @@
+import { Command, flags } from '@oclif/command';
+import chalk from 'chalk';
+import { parseModuleConfig } from '../../utils/files';
+import { checkProjectValidity, parsePageName, toKebabCase } from '../../utils/utilities';
+import { CLI_COMMANDS, CLI_STATE, DOCUMENTATION_LINKS } from '../../utils/constants';
+import catchFunction from '../../functions/catch';
+import { addElementFunction, validityFailed } from '../../functions/addelement';
+
+const TEMPLATE_FOLDERS = ['page'];
+
+export default class Page extends Command {
+  static description = 'add a new Page module.'
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+  }
+
+  static args = [
+    { name: 'name', description: 'name of new page' },
+  ]
+
+  // override Command class error handler
+  catch(error: Error): Promise<any> {
+    return catchFunction(error);
+  }
+
+  async run(): Promise<void> {
+    const { isValid: isValidProject, projectRoot } = checkProjectValidity();
+    // block command unless being run within an rdvue project
+    if (isValidProject === false) {
+        validityFailed(CLI_COMMANDS.AddPage);
+    }
+
+    const { args } = this.parse(Page);
+    const folderList = TEMPLATE_FOLDERS;
+
+    // parse config files required for scaffolding this module
+    const configs = parseModuleConfig(folderList, projectRoot);
+
+    // retrieve page name
+    const pageName = await parsePageName(args);
+
+    // parse config files required for scaffolding this module
+    addElementFunction(TEMPLATE_FOLDERS, projectRoot, pageName)
+
+    this.log(`${CLI_STATE.Success} page added: ${toKebabCase(pageName)}`);
+    this.log(`\n  Visit the documentation page for more info:\n  ${chalk.yellow(DOCUMENTATION_LINKS.Page)}\n`);
+  }
+}
