b2940e86ae0c3fcba9023139d6e264f02dbfc7ad
Andre Vidal <avidal@realdecoy.com>
2023-04-04 21:46:40 +0000

    update init hook

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -9,35 +9,34 @@ const hook: Hook<'init'> = async function ({ id, config, argv }) {
 
   // display namespace help menu if exists
   if (namespace !== undefined && id === undefined) {
-    console.log('NAMESPACE_FOUND', namespace);
     const command = toStandardizedId(namespace as string, config);
 
     await config.runCommand(command, argv);
     // eslint-disable-next-line no-invalid-this
     this.exit();
   } else if (namespace !== undefined && id !== undefined) {
-    console.log(id);
     // A nameespace has been identified and a command has been entered
-    // if the namespace is already included in the command then dont duplicate it
+    // if the namespace is already included in the command then dont duplicate it otherwise include it as a prefix
     const prefix = id.includes(namespace) ? '' : namespace;
     const rawCommandList = id?.split(':');
-    const rawCommand = `${prefix} ${rawCommandList.join(' ')}`;
+    const rawCommand = `${prefix} ${rawCommandList.join(' ')} ${argv.join(' ')}`.trim();
+    const fullCommandList = rawCommand.split(' ');
+    // include the args of the command
+    const rawArgs = fullCommandList.length > 3 ? fullCommandList.slice(3) : [];
+    const fullCommand = fullCommandList.length > 3 ? fullCommandList.slice(0, 3).join(':') : fullCommandList.join(':');
+    const command = toStandardizedId(fullCommand, config);
 
-    const rawArgs = rawCommand.split(' ').splice
-    const command = toStandardizedId(rawCommand, config);
-
-    console.log(`COMMAND_STRING: ${command} ${argv}`);
-
-    await config.runCommand(command, argv);
+    await config.runCommand(command, rawArgs);
     // eslint-disable-next-line no-invalid-this
     this.exit();
   } else if (id !== undefined) {
-    await config.runCommand(id.trim());
+    console.log(`ID: ${id.trim()}`);
+    console.log(`ARG: ${argv}`);
+    await config.runCommand(id.trim(), argv);
     // eslint-disable-next-line no-invalid-this
     this.exit();
   }
 
-  console.log('BLAH');
   // run before the command is identified
 };
 
