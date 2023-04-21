2c57ec05f50a95673e676e24d8b42dbaad82ef1a
andre-vidal <avidal@realdecoy.com>
2023-04-21 12:24:43 -0500

    update

--- docs/.output/public/_nuxt/PropInspector.83153412.js
+++
@@ -1,4 +0,0 @@
-/* empty css                          */import{a as e}from"./Container.f1017aa2.js";import{h as a,q as s,x as n,J as o,u as c}from"./runtime-core.esm-bundler.6894272a.js";import"./index.d2b0cb5e.js";const m={__name:"PropInspector",props:["prop"],setup(r){const p=r,t=a(()=>Array.isArray(p.prop)?"array":typeof p.prop);return(u,i)=>(s(),n("pre",null,"Type: "+o(c(t))+`
-Value:
-`+o(r.prop)+`
-  `,1))}},d=e(m,[["__scopeId","data-v-d8c04239"]]);export{d as default};
