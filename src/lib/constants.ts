f8422dbffeb1acb55a0d2b476b98a8e5f8ca7f48
Mickey <31347338+JamaicanFriedChicken@users.noreply.github.com>
2023-11-23 17:15:44 -0500

    SEPA-849: initial implementation of sentry integration (#76)
    * initial implementation of sentry integration
    
    * Added Sentry functionality as a preset during the project creation
    
    * Rearrange Sentry prompt and loggging
    
    * Added prompt for dsn to sentry plugin when ran standalone
    
    ---------
    
    Co-authored-by: Davane Davis <ddavis@realdecoy.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -53,6 +53,7 @@ enum VUE_CLI_COMMANDS_ENUM {
   PluginElementPlus = 'vue plugin element-plus',
   PluginLocalization = 'vue plugin localization',
   PluginVuetify = 'vue plugin vuetify',
+  PluginSentry = 'vue plugin sentry',
   PluginLibrary = 'vue plugin',
   PoorHelpCommand = 'add-help'
 }
@@ -131,6 +132,7 @@ export const MOBILE_DOCUMENTATION_LINKS = MOBILE_DOCUMENTATION_LINKS_ENUM;
 
 export const VUE_PLUGIN_PRESET_LIST = [
   'Localization (recommended)',
+  'Sentry',
   '[Skip presets]',
 ];
 
